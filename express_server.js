2e5d78d360344c17f63f2860cb1f6a784aad40c9
Arnold Mesa Jr <jayrmesa@gmail.com>
2022-10-27 15:12:36 -0600

    created a URL submission form with GET route to show the Form with POST Requests

--- express_server.js
+++ express_server.js
@@ -2,11 +2,18 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+
+function generateRandomString() {}
+
+//-------------------- DATABASE --------------------\\
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+//-------------------- GET --------------------\\
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -15,6 +22,10 @@ app.get("/", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:id", (req, res) => {
@@ -30,6 +41,17 @@ app.get("/hello", (req, res) => {
   const templateVars = { greeting: "Hello World!" };
   res.render("hello_world", templateVars);
 });
+
+
+//-------------------- POST --------------------\\
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
+
+//-------------------- LISTEN --------------------\\
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
