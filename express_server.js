274624340c136c580d82743ad5f4d1fe74992f29
Arnold Mesa Jr <jayrmesa@gmail.com>
2022-10-30 18:10:00 -0600

    Implemented an update/edit button for urls, also fix an issue where the longURl are not showing when user created one. Created a randomString for the shortURL

--- express_server.js
+++ express_server.js
@@ -1,10 +1,26 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+
 app.set("view engine", "ejs");
+
 app.use(express.urlencoded({ extended: true }));
 
-function generateRandomString() {}
+
+// Functions for random string for short URL
+
+function generateRandomString() {
+  const randoms = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  const ranlength = 6;
+  let ranString = "";
+
+  for (let i = 0; i < ranlength; i++) {
+    
+    const ranNumber = Math.floor(Math.random() * randoms.length);
+    ranString += randoms[ranNumber];
+  }
+  return ranString;
+}
 
 //-------------------- DATABASE --------------------\\
 
@@ -20,6 +36,10 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
@@ -29,38 +49,36 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
-app.get("/urls/:id", (req, res) => {
-  const longURL = urlDatabase[req.params.id];
+app.get("/u/:id", (req, res) => {
+  const shortURL = req.params.id;
+  const longURL = urlDatabase[shortURL];
   res.redirect(longURL);
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  const templateVars = { greeting: "Hello World!" };
-  res.render("hello_world", templateVars);
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  res.render("urls_show", templateVars);
 });
 
 
 //-------------------- POST --------------------\\
 
 app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  const id = generateRandomString();
+  urlDatabase[id] = req.body.longURL;
+  const templateVars = { id: id, longURL: urlDatabase[id] };
+  res.render("urls_show", templateVars);
+});
+
+app.post("/urls/:id/edit", (req, res) => {
+  urlDatabase[req.params.id] = req.body.longURL;
+  res.redirect('/urls');
 });
 
 app.post("/urls/:id/delete", (req, res) => {
   delete urlDatabase[req.params.id];
   res.redirect('/urls');
 });
-
-app.post("/urls/:id/", (req, res) => {
-  urlDatabase[req.params.id] = req.body.longURL;
-  res.redirect('/urls');
-});
-
 
 
 //-------------------- LISTEN --------------------\\
