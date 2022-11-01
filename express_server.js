cb55ba7eaa0d3b651fb16b8ef03e62a600de7f6a
Arnold Mesa Jr <jayrmesa@gmail.com>
2022-11-01 15:39:13 -0600

    added a registration form

--- express_server.js
+++ express_server.js
@@ -1,16 +1,49 @@
 const express = require("express");
 const cookieParser = require('cookie-parser');
+
+//////////////////////////////////////////////////////////////////////
+/// Configuration/SETUP
+//////////////////////////////////////////////////////////////////////
+
 const app = express();
 const PORT = 8080; // default port 8080
 
-app.set("view engine", "ejs");
+//////////////////////////////////////////////////////////////////////
+/// MIDDLEWARE
+//////////////////////////////////////////////////////////////////////
 
+app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+//////////////////////////////////////////////////////////////////////
+/// DATABASE 
+//////////////////////////////////////////////////////////////////////
 
-// Functions for random string for short URL
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com",
+};
 
+// example users according to compass
+const users = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur",
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk",
+  },
+};
+
+//////////////////////////////////////////////////////////////////////
+/// Functions 
+//////////////////////////////////////////////////////////////////////
+
+//for random string for short URL
 function generateRandomString() {
   const randoms = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
   const ranlength = 6;
@@ -24,18 +57,27 @@ function generateRandomString() {
   return ranString;
 }
 
-//-------------------- DATABASE --------------------\\
-
-const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com",
+//Will search the email if it already exist, Return null if found
+const getUserByEmail = function(email) {
+  for (const id of Object.keys(users)) {
+    if (users[id]['email'] === email) {
+      return users[id];
+    }
+  }
+  return null;
 };
 
 
-//-------------------- GET --------------------\\
+//////////////////////////////////////////////////////////////////////
+/// Routes - GET
+//////////////////////////////////////////////////////////////////////
 
 app.get("/", (req, res) => {
   res.send("Hello!");
+});
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body></b>Hello World</b></body></html>\n");
 });
 
 app.get("/urls.json", (req, res) => {
@@ -43,12 +85,17 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
+  const templateVars = { 
+    urls: urlDatabase, 
+    username: users[req.cookies["user_id"]] 
+  };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  const templateVars = { username: req.cookies["username"] };
+  const templateVars = { 
+    user: users[req.cookies["user_id"]] 
+  };
   res.render("urls_new", templateVars);
 });
 
@@ -59,17 +106,29 @@ app.get("/u/:id", (req, res) => {
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], username: req.cookies["username"] };
+  const templateVars = { 
+    id: req.params.id, 
+    longURL: urlDatabase[req.params.id], 
+    user: users[req.cookies["user_id"]] 
+  };
   res.render("urls_show", templateVars);
 });
 
+app.get("/register",(req, res) => {
+  const templateVars = {
+    user: users[req.cookies["user_id"]]
+  };
+  res.render("urls_register", templateVars);
+});
 
-//-------------------- POST --------------------\\
+//////////////////////////////////////////////////////////////////////
+/// Routes - POST
+//////////////////////////////////////////////////////////////////////
 
 app.post("/urls", (req, res) => {
-  const id = generateRandomString();
-  urlDatabase[id] = req.body.longURL;
-  const templateVars = { id: id, longURL: urlDatabase[id] };
+  const shortURL = generateRandomString();
+  const longURL = req.body.longURL;
+  urlDatabase[shortURL] = longURL;
   res.render("urls_show", templateVars);
 });
 
@@ -94,7 +153,9 @@ app.post("/urls/:id/delete", (req, res) => {
 });
 
 
-//-------------------- LISTEN --------------------\\
+//////////////////////////////////////////////////////////////////////
+/// LISTENER
+//////////////////////////////////////////////////////////////////////
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
